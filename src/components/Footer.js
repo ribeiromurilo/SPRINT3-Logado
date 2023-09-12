98fbb96641c9589e12ca80f668787f7cf83984a3
feliperfo <550858@fiap.com.br>
2023-09-11 22:57:32 -0300

    vistoria page

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,15 +4,16 @@ import './Footer.css';
 function Footer() {
   return (
     <div className='footer-container'>
-      <section className='footer-subscription'>
-        <p className='footer-subscription-heading'>
+      <section className='footer-nomes'>
+        <p className='footer-nomes-heading'>
           1TDSB <br />
-          Ana Júlia Almeida Silva Neves – RM: 98974 <br />
+          Ana Júlia Almeida Silva Neves – RM: 98974 (1TDSS) <br />
           Felipe Rodrigues Franco de Oliveira – RM: 552101 <br />
           Lucas Pinheiro de Melo – RM: 97707 <br />
           Murilo Ribeiro Valério da Silva – RM: 550858 <br />
           Sarah Santos Teixeira – RM: 550635
         </p>
+        <a href="https://www.github.com/ribeiromurilo/SPRINT3" target="_blank">Repositório</a>
       </section>
     </div>
   );
--- src/components/pages/Vistoria.js
+++ src/components/pages/Vistoria.js
@@ -1,6 +1,29 @@
 import React from 'react';
-import '../../App.css';
+import "./Vistoria.css";
+import Footer from '../Footer';
 
-export default function Vistoria() {
-  return <h1 className='vistoria'>Vistoria</h1>;
+const Vistoria = () => {
+  return (
+    <div className="container">
+      <div className="content">
+        <h1>Vistoria</h1>
+        <h2>Olá, <strong>Usuário!</strong></h2>
+        <p>Ainda não concluímos o resultado de sua vistoria. <br />
+          Caso não for aprovado, você será permitido à mais 2 tentativas,
+          após o limite será necessário contatar a Porto.
+          Por favor, aguarde por breves atualizações.</p>
+      </div>
+    </div>
+  );
 }
+
+const App = () => {
+  return (
+    <>
+      <Vistoria />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
